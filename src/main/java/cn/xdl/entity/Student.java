2a81943c15a7da366ce8fc214195f7a0e0c5d326
jack-qiang <jack-qiang@qq.com>
2019-11-14 17:16:36 +0800

    增加了age属性

--- src/main/java/cn/xdl/entity/Student.java
+++ src/main/java/cn/xdl/entity/Student.java
@@ -3,8 +3,17 @@ package cn.xdl.entity;
 public class Student {
 	private int id;
 	private String name;
+	private int age;
 	
 	
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
