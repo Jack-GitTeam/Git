9295c06d84a8659d409c32074129919b6b306270
jack-zyq <2917452157@qq.com>
2019-11-14 17:05:54 +0800

    增加了属性

--- src/main/java/cn/xdl/entity/Teacher.java
+++ src/main/java/cn/xdl/entity/Teacher.java
@@ -1,25 +1,19 @@
 package cn.xdl.entity;
 
-public class Student {
+public class Teacher {
 	private int id;
 	private String name;
-	
-	
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
